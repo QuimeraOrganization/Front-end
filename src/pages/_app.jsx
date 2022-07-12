c47cf4a1a127182f13230b8a27bc7e926b111634
Eros Linnyker <linnykereros@gmail.com>
2022-07-12 12:50:38 -0300

    Autenticação

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "../context/AuthContext";
 
-import theme from '../styles/theme';
+import theme from "../styles/theme";
 import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      {/* Depois modificar para não aparecer a barra de navegação no login e no cadastro */}
-      <NavBar />
-      <Component {...pageProps} />  
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        {/* Depois modificar para não aparecer a barra de navegação no login e no cadastro */}
+        <NavBar />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   );
 }
 
