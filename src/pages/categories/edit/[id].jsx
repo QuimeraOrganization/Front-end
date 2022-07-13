021c78e0bab80c601acd0c504e6b2cf46d30dc9d
Eros Linnyker <linnykereros@gmail.com>
2022-07-13 14:03:14 -0300

    Pag admin categorias + crud

---
+++ src/pages/categories/edit/[id].jsx
@@ -0,0 +1,125 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Divider,
+  VStack,
+  SimpleGrid,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { parseCookies } from "nookies";
+import { useEffect, useState } from "react";
+import { Input } from "../../../components/Form/Input";
+import SideBar from "../../../components/SideBar";
+import Router from "next/router";
+import {
+  getCategoryById,
+  updateCategory,
+} from "../../../services/categoryService";
+import { toast } from "react-toastify";
+
+export default function EditUser({ categoryId }) {
+  const [category, setCategory] = useState({
+    name: "",
+  });
+
+  useEffect(() => {
+    getCategoryById(categoryId).then(({ name }) => {
+      setCategory({
+        name,
+      });
+    });
+  }, []);
+  const handleUpdateCategory = async () => {
+    try {
+      await updateCategory(categoryId, category.name);
+
+      toast.success("Categoria atualizada com sucesso", {
+        autoClose: 2000,
+      });
+      Router.push("/categories");
+    } catch (err) {
+      // console.log(ingredient.name);
+      if (!category.name) {
+        toast.error("Nome do ingrediente obrigatório!", {
+          autoClose: 2000,
+        });
+      }
+    }
+  };
+
+  return (
+    <Box>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+        <Box
+          width="1010px"
+          //  height="700px"
+          marginLeft="50px"
+          flex="1"
+          borderRadius={8}
+          bg="#F2F1F1"
+          p={["6", "8"]}
+        >
+          <Heading size="lg" fontWeight="700">
+            Editar Categoria
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+          <VStack spacing={["6", "8"]}>
+            <SimpleGrid minChildWidth="248px" spacing={["6", "8"]} w="100%">
+              <Input
+                value={category.name}
+                name="category"
+                type="text"
+                label="Nome"
+                onChange={(e) =>
+                  setCategory({ ...category, name: e.target.value })
+                }
+              />
+            </SimpleGrid>
+          </VStack>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/categories" passHref>
+                <Button as="a" bg="grey" color="#ffffff">
+                  Cancelar
+                </Button>
+              </Link>
+              <Button
+                bg="#6FBE5E"
+                color="#ffffff"
+                onClick={handleUpdateCategory}
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const cookies = parseCookies(context);
+
+  const token = cookies["nextauth.token"];
+  //se não existir o token, ele redireciona para a pag index.
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  console.log(context.query.id);
+  return {
+    props: {
+      categoryId: context.query.id,
+    }, // will be passed to the page component as props
+    //sempre tem que passar o componente props, mesmo que seja vazio.
+  };
+}
---
+++ src/pages/categories/index.jsx
@@ -0,0 +1,164 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+  Divider,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+import { parseCookies } from "nookies";
+import { useEffect, useState } from "react";
+import { RiAddLine, RiDeleteBinLine, RiPencilLine } from "react-icons/ri";
+import Link from "next/link";
+
+import axios from "../../config/axios";
+import SideBar from "../../components/SideBar/index";
+import {
+  getAllCategories,
+  deleteCategory,
+} from "../../services/categoryService";
+export default function UserList(props) {
+  const [categories, setCategories] = useState(props.categories);
+
+  //breakpoint de responsividade
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  const handleDelete = async (id) => {
+    await deleteCategory(id);
+    setCategories(await getAllCategories());
+  };
+
+  return (
+    <Box>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+        <Box
+          width="1010px"
+          //  height="700px"
+          marginLeft="50px"
+          flex="1"
+          borderRadius={8}
+          bg="#F2F1F1"
+          p="8"
+        >
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Categorias
+            </Heading>
+            <Link href="/categories/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                bg="#6FBE5E"
+                colorScheme="#FFFFFF"
+                cursor="pointer"
+                _hover={{ bg: "green.400" }}
+                leftIcon={<Icon as={RiAddLine} />}
+              >
+                Adicionar
+              </Button>
+            </Link>
+          </Flex>
+          <Divider my="6" borderColor="gray.700" />
+          <Table>
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray" width="32px"></Th>
+                <Th>Categoria</Th>
+                {isWideVersion && <Th>Permission</Th>}
+                <Th>ID</Th>
+                <Th width="1px"></Th>
+                <Th width="1px"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {categories.map((category) => (
+                <Tr key={category.id}>
+                  <Td px={["4", "4", "6"]}></Td>
+                  <Td>
+                    <Box>
+                      <Text fontSize="sm">{category.name}</Text>
+                    </Box>
+                  </Td>
+                  {isWideVersion && <Td>{category.name}</Td>}
+                  <Td>{category.id}</Td>
+
+                  <Td>
+                    <Link href={`/categories/edit/${category.id}`}>
+                      <Button
+                        as="a"
+                        left="10px"
+                        size="sm"
+                        fontSize="sm"
+                        bg="#6FBE5E"
+                        colorScheme="#FFFFFF"
+                        cursor="pointer"
+                        _hover={{ bg: "green.400" }}
+                        leftIcon={<Icon as={RiPencilLine} />}
+                      >
+                        Editar
+                      </Button>
+                    </Link>
+                  </Td>
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      bg="#6FBE5E"
+                      colorScheme="#FFFFFF"
+                      cursor="pointer"
+                      _hover={{ bg: "green.400" }}
+                      onClick={() => handleDelete(category.id)}
+                      leftIcon={<Icon as={RiDeleteBinLine} />}
+                    >
+                      Excluir
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+//método executado no lado do servidor, quando o user acessar a página;
+//nesse caso o next faz um get na minha api antes de rendezirar a pagina, ou seja
+//antes de aparecer qualquer tipo de interface
+export async function getServerSideProps(context) {
+  const cookies = parseCookies(context);
+
+  const token = cookies["nextauth.token"];
+  //se não existir o token, ele redireciona para a pag index.
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  const response = await axios.get("/categories");
+  return {
+    props: {
+      categories: response.data,
+    }, // will be passed to the page component as props
+    //sempre tem que passar o componente props, mesmo que seja vazio.
+  };
+}
