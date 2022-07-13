2a3021aba420c447e596e42ed47a9facb326ea94
Nathercia <dev.nathercia@gmail.com>
2022-07-13 15:46:50 -0300

    create feedbackService.js

---
+++ src/services/feedbackService.js
@@ -0,0 +1,26 @@
+import axios from "../config/axios";
+
+async function createFeedback(contents, productId, userId) {
+  const feedback = await axios.post("/feedbacks", { contents, productId, userId });
+  return feedback.data;
+}
+async function deleteFeedback(id) {
+  return await axios.delete(`/feedbacks/${id}`);
+}
+
+async function getFeedbacks() { 
+  const feedbacks = await axios.get("/feedbacks");  
+  return feedbacks.data;
+}
+
+async function updateFeedback(id, contents, userId, productId, permission) {
+  const feedback = await axios.put(`/feedbacks/${id}`, { contents, userId, productId, permission });
+  return feedback.data;
+}
+
+async function getFeedbackById(id) {
+  const feedbacks = await axios.get(`/feedbacks/${id}`);
+  return feedbacks.data;
+}
+
+export { createFeedback, deleteFeedback, getFeedbacks, updateFeedback, getFeedbackById };
