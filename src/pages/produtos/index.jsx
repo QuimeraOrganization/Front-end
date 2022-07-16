497474ff7075fd7f32e7d5948978c1823dd3fdac
Eros Linnyker <linnykereros@gmail.com>
2022-07-16 08:20:56 -0300

    Focus input

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,12 +1,36 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { Flex, Spinner, HStack, VStack, Input, InputGroup, InputRightAddon, Select, Button, Tooltip, useDisclosure } from '@chakra-ui/react';
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from '@chakra-ui/react'
-import { SearchIcon, ChevronLeftIcon, ChevronRightIcon, AddIcon } from '@chakra-ui/icons';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import {
+  Flex,
+  Spinner,
+  HStack,
+  VStack,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Select,
+  Button,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import {
+  SearchIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  AddIcon,
+} from "@chakra-ui/icons";
 
 import Card from "../../components/Card";
 import { getProductsPaged } from "../../services/productService";
-import ProductForm from '../../components/forms/ProductForm';
+import ProductForm from "../../components/forms/ProductForm";
 
 export default function Home() {
   const router = useRouter();
@@ -41,7 +65,7 @@ export default function Home() {
   }
 
   function handleCardClick(productId) {
-    router.push(`produtos/${productId}`)
+    router.push(`produtos/${productId}`);
   }
 
   return (
@@ -57,27 +81,15 @@ export default function Home() {
         </ModalContent>
       </Modal>
 
-      <VStack
-        px={10}
-        mt={4}
-      >
-        <HStack
-          width="100%"
-          justify="space-between"
-          mb={4}
-        >
-          <HStack
-            width="100%"
-          >
+      <VStack px={10} mt={4}>
+        <HStack width="100%" justify="space-between" mb={4}>
+          <HStack width="100%">
             {/* Barra de busca */}
-            <InputGroup
-              size='sm'
-              width="30%"
-              border="0px solid #6FBE5E"
-            >
+            <InputGroup size="sm" width="30%" border="0px solid #6FBE5E">
               <Input
-                placeholder='Busque por um produto...'
-                fontSize={{ base: '11px', md: '12px', lg: '13px' }}
+                focusBorderColor="#6FBE5E"
+                placeholder="Busque por um produto..."
+                fontSize={{ base: "11px", md: "12px", lg: "13px" }}
                 borderRadius={200}
               />
               <InputRightAddon
@@ -92,7 +104,7 @@ export default function Home() {
               hasArrow
               label="Cadastrar produto"
               backgroundColor="#6FBE5E"
-              fontSize={['11px', '12px', '13px']}
+              fontSize={["11px", "12px", "13px"]}
             >
               <Button
                 minWidth="auto"
@@ -109,34 +121,29 @@ export default function Home() {
 
           {/* Filtro */}
           <Select
-            size='sm'
-            fontSize={{ base: '11px', md: '12px', lg: '13px' }}
+            size="sm"
+            fontSize={{ base: "11px", md: "12px", lg: "13px" }}
             width="20%"
-            placeholder='Filtrar por'
+            placeholder="Filtrar por"
             backgroundColor="#fff"
             border="1px solid"
             borderColor="#6FBE5E"
             borderRadius={6}
           >
-            <option value='option1'>Verificado</option>
-            <option value='option2'>Nome crescente</option>
-            <option value='option3'>Nome decresente</option>
+            <option value="option1">Verificado</option>
+            <option value="option2">Nome crescente</option>
+            <option value="option3">Nome decresente</option>
           </Select>
         </HStack>
 
-        {loading && (
-          <Spinner color="#6FBE5E" />
-        )}
+        {loading && <Spinner color="#6FBE5E" />}
 
         <HStack>
-          <Flex
-            direction="row"
-            justify="center"
-            wrap="wrap"
-          >
-            {
-              pageProducts != null && pageProducts.data != null && !loading &&
-              (pageProducts.data.map(product => {
+          <Flex direction="row" justify="center" wrap="wrap">
+            {pageProducts != null &&
+              pageProducts.data != null &&
+              !loading &&
+              pageProducts.data.map((product) => {
                 return (
                   <Card
                     key={product.id}
@@ -147,32 +154,27 @@ export default function Home() {
                     onClick={(e) => handleCardClick(product.id)}
                   />
                 );
-              }))
-            }
+              })}
           </Flex>
         </HStack>
 
         <HStack>
           <Button
             disabled={currentPage - 1 < 1 ? true : false}
-            size='sm'
+            size="sm"
             my={2}
             backgroundColor="#fff"
             border="1px solid"
             borderColor="#6FBE5E"
             onClick={() => prevPage()}
           >
-            <ChevronLeftIcon
-              color="#6FBE5E"
-              width="100%"
-              height="100%"
-            />
+            <ChevronLeftIcon color="#6FBE5E" width="100%" height="100%" />
           </Button>
 
           <Button
             disabled={false}
             cursor="default"
-            size='sm'
+            size="sm"
             my={2}
             backgroundColor="#fff"
             border="1px solid"
@@ -184,21 +186,17 @@ export default function Home() {
 
           <Button
             disabled={currentPage + 1 > pageProducts.totalPages ? true : false}
-            size='sm'
+            size="sm"
             my={2}
             backgroundColor="#fff"
             border="1px solid"
             borderColor="#6FBE5E"
             onClick={() => nextPage()}
           >
-            <ChevronRightIcon
-              color="#6FBE5E"
-              width="100%"
-              height="100%"
-            />
+            <ChevronRightIcon color="#6FBE5E" width="100%" height="100%" />
           </Button>
         </HStack>
-      </VStack >
+      </VStack>
     </>
   );
 }
